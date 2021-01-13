3479fbf583d4cdade708ae0aa1cefde358e870f7
avt.avt.avt1 <avadrot@myges.fr>
2021-01-13 12:31:07 +0100

    Creation new Release
    Ajout dans le screen/App.js d'un texte informant le passage en release

--- src/screens/App.js
+++ src/screens/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/screen/App.js</code> and save to reload.
+          Edit <code>src/screen/App.js</code> and save to reload. New Release Version 1.
         </p>
         <a
           className="App-link"
